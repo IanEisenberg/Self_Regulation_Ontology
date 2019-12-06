c933beddc580b8b135adad7279c865cf67e44632
ian <ianeisenberg90@gmail.com>
2019-12-06 08:19:18 -0800

    added singularity_image transfer script and folder

---
+++ singularity_images/README.md
@@ -0,0 +1,3 @@
+first run docker2singularity.sh from home directory
+
+then run transfer_image.sh
---
+++ singularity_images/transfer_image.sh
@@ -0,0 +1,3 @@
+scp *img ieisenbe@dtn.sherlock.stanford.edu:/oak/stanford/groups/russpold/data/uh2/aim1_mturk_behavioral_data/singularity_images/
+
+
